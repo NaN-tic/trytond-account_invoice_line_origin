09fff43202f761acae12c3bb3b734fe882b3b32b
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-28 16:55:49 +0200

    Add test extras

--- tests/test_module.py
+++ tests/test_module.py
@@ -9,6 +9,7 @@ from trytond.tests.test_tryton import ModuleTestCase
 class AccountInvoiceLineOriginTestCase(CompanyTestMixin, ModuleTestCase):
     'Test AccountInvoiceLineOrigin module'
     module = 'account_invoice_line_origin'
+    extras = ['account_invoice_stock', 'purchase', 'sale']
 
 
 del ModuleTestCase
